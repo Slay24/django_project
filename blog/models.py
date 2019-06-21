5fb53664d289b464917f976c55fc72e44aedc261
Dkass <d.kassaye@outlook.com>
2019-06-20 22:55:57 -0500

    Post model works

--- blog/models.py
+++ blog/models.py
@@ -6,5 +6,5 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateField(default=timezone)
+    date_posted = models.DateField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
